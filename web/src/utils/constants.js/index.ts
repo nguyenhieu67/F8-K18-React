6a2cc5102c9bcaeebd6802393d23fdcd287c8d00
nguyenhieu67 <149569751+nguyenhieu67@users.noreply.github.com>
2026-06-20 16:55:53 +0900

    fix: build mode

--- web/src/utils/constants.js/index.ts
+++ web/src/utils/constants.js/index.ts
@@ -1,10 +1,10 @@
 let apiRoot = "";
 
-if (import.meta.env.BUILD_MODE === "dev") {
+if (import.meta.env.VITE_BUILD_MODE === "dev") {
   apiRoot = "http://localhost:3001";
 }
 
-if (import.meta.env.BUILD_MODE === "production") {
+if (import.meta.env.VITE_BUILD_MODE === "production") {
   apiRoot =
     import.meta.env.VITE_API_URL || "https://trello-clone-api.onrender.com/v1";
 }
